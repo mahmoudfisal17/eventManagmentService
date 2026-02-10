ecad44dac84ac7ca5874732966c1bf5005afb6cd
mahmoudfisal17 <mahmoudfisal17@gmail.com>
2026-02-10 17:39:08 +0200

    FIX

--- src/main/java/com/example/eventManagmentService/configration/KafkaProducerConfig.java
+++ src/main/java/com/example/eventManagmentService/configration/KafkaProducerConfig.java
@@ -1,9 +1,11 @@
 package com.example.eventManagmentService.configration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
+@Configuration
 public class KafkaProducerConfig {
 
     @Bean
--- src/main/java/com/example/eventManagmentService/configration/WebClientConfig.java
+++ src/main/java/com/example/eventManagmentService/configration/WebClientConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
-    @LoadBalanced
+  //  @LoadBalanced
     public WebClient.Builder webClient() {
         return WebClient.builder();
     }
